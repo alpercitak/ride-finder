78ab0615cff6215f4746c01f898115961a87dd20
Alper Çıtak <citak.alper@gmail.com>
2022-10-22 19:28:06 +0300

    prometheus setup
    added prometheus as metrics provider
    added missing packages for ts-eslint

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,27 @@
 import express, { Express, Request, Response } from 'express';
 import * as dotenv from 'dotenv';
+import promBundle from 'express-prom-bundle';
 
 dotenv.config();
 
 const app: Express = express();
 const port: number = parseInt(process.env.PORT as string) || 8001;
+
+/**
+ * Initialize prometheus and mount to the path /metrics
+ */
+const metricsMiddleware = promBundle({
+  includeMethod: true,
+  includePath: true,
+  includeStatusCode: true,
+  includeUp: true,
+  customLabels: { project_name: 'ride-finder', project_type: 'api' },
+  promClient: {
+    collectDefaultMetrics: {},
+  },
+  metricsPath: '/metrics',
+});
+app.use(metricsMiddleware);
 
 /**
  * Endpoint root
