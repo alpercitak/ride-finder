1c5375f35ab373148a31c9e3631c0f5cc938b45b
Alper Çıtak <citak.alper@gmail.com>
2022-10-22 20:44:31 +0300

    endpoint implementation for /rides and /prices

---
+++ src/__tests__/index.test.ts
@@ -0,0 +1,160 @@
+const request = require('supertest');
+
+describe('index', () => {
+  let server: any;
+  beforeAll(async () => {
+    process.env.PORT = undefined;
+    const mod = await import('../index');
+    server = (mod as any).default;
+  });
+
+  afterAll((done) => {
+    if (server) {
+      server.close(done);
+    }
+  });
+
+  it('[/] should return Hello', async () => {
+    const res = await request(server).get('/');
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toEqual({ message: 'Hello' });
+  });
+
+  it('[/rides] should return rides', async () => {
+    const res = await request(server).get('/rides?limit=10');
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toHaveProperty('data');
+    expect(res.body.data).toBeInstanceOf(Array);
+  });
+
+  it('[/rides] should be all: disabled [true]', async () => {
+    const res = await request(server).get('/rides?limit=10&is_disabled=true');
+    const length = res.body.data.reduce((acc: number, val: any) => {
+      return val.is_disabled ? ++acc : acc;
+    }, 0);
+    expect(res.body.data.length).toEqual(length);
+  });
+
+  it('[/rides] should be all: reserved [false]', async () => {
+    const res = await request(server).get('/rides?limit=10&is_reserved=false');
+    const length = res.body.data.reduce((acc: number, val: any) => {
+      return !val.is_reserved ? ++acc : acc;
+    }, 0);
+    expect(res.body.data.length).toEqual(length);
+  });
+
+  it('[/rides] should return rides [with_prices]', async () => {
+    const res = await request(server).get('/rides?limit=10&with_prices=true');
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toHaveProperty('data');
+    expect(res.body.data).toBeInstanceOf(Array);
+
+    const length = res.body.data.reduce((acc: number, ride: any) => {
+      return ride['pricing_plan'] ? ++acc : acc;
+    }, 0);
+
+    expect(res.body.data.length).toEqual(length);
+  });
+
+  it('[/rides] should return rides [cluster_size]', async () => {
+    const length = 2;
+    const res = await request(server).get(`/rides?limit=10&cluster_size=${length}`);
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toHaveProperty('data');
+    expect(res.body.data).toBeInstanceOf(Array);
+
+    const clusterLength = res.body.data.length;
+    expect(clusterLength).toEqual(length);
+  });
+
+  it('[/rides] should return rides [center & radius]', async () => {
+    const center = '48.839768,2.309268';
+    const radius = 1000;
+    const res = await request(server).get(`/rides?limit=10&center=${center}&radius=${radius}`);
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toHaveProperty('data');
+    expect(res.body.data).toBeInstanceOf(Array);
+  });
+
+  it('[/rides] should return error [center & !radius]', async () => {
+    const center = '48.839768,2.309268';
+    const res = await request(server).get(`/rides?limit=10&center=${center}`);
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toHaveProperty('error');
+  });
+
+  it('[/rides] should return error [!center & radius]', async () => {
+    const radius = 1000;
+    const res = await request(server).get(`/rides?limit=10&radius=${radius}`);
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toHaveProperty('error');
+  });
+
+  it('[/rides] should return error [radius]', async () => {
+    const center = '48.839768,2.309268';
+    const radius = -1;
+    const res = await request(server).get(`/rides?limit=10&center=${center}&radius=${radius}`);
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toHaveProperty('error');
+  });
+
+  it('[/rides] should return rides [polygon]', async () => {
+    const res = await request(server).get(
+      `/rides?limit=10&polygon[]=48.0,2.0&polygon[]=48.0,2.27&polygon[]=48.9,2.27&polygon[]=48.9,2.31`
+    );
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toHaveProperty('data');
+    expect(res.body.data).toBeInstanceOf(Array);
+  });
+
+  it('[/rides] should return error [limit]', async () => {
+    const res = await request(server).get(`/rides?limit=-1`);
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toHaveProperty('error');
+  });
+
+  it('[/rides] should return error [offset]', async () => {
+    const res = await request(server).get(`/rides?offset=-1`);
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toHaveProperty('error');
+  });
+
+  it('[/rides] should return error [cluster_size]', async () => {
+    const res = await request(server).get(`/rides?cluster_size=-1`);
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toHaveProperty('error');
+  });
+
+  it('[/prices] should return prices', async () => {
+    const res = await request(server).get(`/prices`);
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toHaveProperty('data');
+  });
+
+  it('[/prices] should return prices []', async () => {
+    const res = await request(server).get(`/prices`);
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toHaveProperty('data');
+  });
+
+  it('[/prices] should be all: plan_id [a4d5e9f0-3f82-459b-901f-41ef42fd2402]', async () => {
+    const plan_id = 'a4d5e9f0-3f82-459b-901f-41ef42fd2402';
+    const res = await request(server).get(`/prices?plan_id=${plan_id}`);
+    expect(res.body).toHaveProperty('data');
+
+    const length = res.body.data.reduce((acc: number, price: any) => {
+      return price.plan_id === plan_id ? ++acc : acc;
+    }, 0);
+    expect(res.body.data.length).toEqual(length);
+  });
+
+  it('[/health] should return 200', async () => {
+    const res = await request(server).get(`/health`);
+    expect(res.statusCode).toEqual(200);
+  });
+
+  it('[/metrics] should return 200', async () => {
+    const res = await request(server).get(`/metrics`);
+    expect(res.statusCode).toEqual(200);
+  });
+});
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import express, { Express, Request, Response } from 'express';
 import * as dotenv from 'dotenv';
+import { getRides, GetRidesRequest, getPrices, GetPricesRequest } from './lib/service';
+import { GeoCoordinates } from './lib/geo';
 import promBundle from 'express-prom-bundle';
 
 dotenv.config();
@@ -24,10 +26,97 @@ const metricsMiddleware = promBundle({
 app.use(metricsMiddleware);
 
 /**
+ * Convers the given value to boolean
+ * @param value Value to be checked
+ * @returns boolean
+ */
+const checkBoolean = (value: any): boolean => {
+  return !(value === 'false' || value === '0' || value === '' || value === undefined);
+};
+
+/**
+ * Checks if the number is a positive integer or zero
+ * @param value Value to be checked
+ * @returns boolean
+ */
+const isValidNumber = (value: number): boolean => {
+  return !isNaN(value) && value >= 0;
+};
+
+/**
  * Endpoint root
  */
 app.get('/', (req: Request, res: Response) => {
   return res.status(200).json({ message: 'Hello' });
+});
+
+/**
+ * Endpoint for retrieving rides
+ */
+app.get('/rides', async (req: Request<{}, {}, {}, GetRidesRequest>, res: Response): Promise<Response> => {
+  req.query.is_disabled = req.query.is_disabled ? checkBoolean(req.query.is_disabled) : undefined;
+  req.query.is_reserved = req.query.is_reserved ? checkBoolean(req.query.is_reserved) : undefined;
+  req.query.with_prices = req.query.with_prices ? checkBoolean(req.query.with_prices) : undefined;
+
+  if (req.query.limit !== undefined) {
+    req.query.limit = Number(req.query.limit);
+    if (!isValidNumber(req.query.limit)) {
+      return res.status(200).json({ error: '[limit] must be >= 0 and a valid number' });
+    }
+  }
+
+  if (req.query.offset !== undefined) {
+    req.query.offset = Number(req.query.offset);
+    if (!isValidNumber(req.query.offset)) {
+      return res.status(200).json({ error: '[offset] must be >= 0 and a valid number' });
+    }
+  }
+
+  if (req.query.cluster_size !== undefined) {
+    req.query.cluster_size = Number(req.query.cluster_size);
+    if (!isValidNumber(req.query.cluster_size)) {
+      return res.status(200).json({ error: '[cluster_size] must be >= 0 and a valid number' });
+    }
+  }
+
+  if (req.query.polygon) {
+    req.query.polygon = req.query.polygon.map((x) => {
+      const arrCoordinates = x.toString().split(',');
+      return new GeoCoordinates(parseFloat(arrCoordinates[0]), parseFloat(arrCoordinates[1]));
+    });
+  }
+
+  if (req.query.center || req.query.radius) {
+    if (req.query.center && !req.query.radius) {
+      return res.status(200).json({ error: '[radius] is mandatory when [center] is provided' });
+    }
+    if (req.query.radius && !req.query.center) {
+      return res.status(200).json({ error: '[center] is mandatory when [radius] is provided' });
+    }
+
+    if (req.query.center) {
+      const arrCoordinates = req.query.center.toString().split(',');
+      req.query.center = new GeoCoordinates(parseFloat(arrCoordinates[0]), parseFloat(arrCoordinates[1]));
+    }
+
+    if (req.query.radius) {
+      req.query.radius = Number(req.query.radius);
+      if (!isValidNumber(req.query.radius)) {
+        return res.status(200).json({ error: '[radius] must be >= 0 and a valid number' });
+      }
+    }
+  }
+
+  const response = await getRides(req.query);
+  return res.status(200).json({ data: response });
+});
+
+/**
+ * Endpoint for retrieving prices
+ */
+app.get('/prices', async (req: Request<{}, {}, {}, GetPricesRequest>, res: Response): Promise<Response> => {
+  const response = await getPrices(req.query);
+  return res.status(200).json({ data: response });
 });
 
 /**
