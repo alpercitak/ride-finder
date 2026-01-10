1aa20781b0348376f0339418f19d3ed7c3f8b5e9
Alper Çıtak <citak.alper@gmail.com>
2026-01-10 23:49:49 +0100

    set API_BASE_URL

--- src/gbfs/index.ts
+++ src/gbfs/index.ts
@@ -4,6 +4,8 @@ import type { ApiRequest } from './types';
 
 const cache = new nodeCache();
 
+const API_BASE_URL: string = 'https://data-sharing.tier-services.io/tier_paris/gbfs/2.2';
+
 /**
  * Returns the response from specific api
  * @param ApiRequest.apiUrl - Url to be called
@@ -37,38 +39,35 @@ const makeRequest = async ({ apiUrl, cacheKey, rawResponse }: ApiRequest): Promi
  * @param rawResponse - Should return all response or just data
  * @returns Defined services
  */
-const getServices = async (rawResponse: boolean = false) => {
-  return makeRequest({
-    apiUrl: 'https://data-sharing.tier-services.io/tier_paris/gbfs/2.2',
+const getServices = async (rawResponse: boolean = false) =>
+  makeRequest({
+    apiUrl: `${API_BASE_URL}`,
     cacheKey: 'get-services',
     rawResponse,
   });
-};
 
 /**
  * Returns the pricing plans
  * @param rawResponse - Should return all response or just data
  * @returns Defined pricing plans
  */
-const getPricingPlans = async (rawResponse: boolean = false) => {
-  return makeRequest({
-    apiUrl: 'https://data-sharing.tier-services.io/tier_paris/gbfs/2.2/system-pricing-plans',
+const getPricingPlans = async (rawResponse: boolean = false) =>
+  makeRequest({
+    apiUrl: `${API_BASE_URL}/system-pricing-plans`,
     cacheKey: 'get-pricing-plans',
     rawResponse,
   });
-};
 
 /**
  * Returns the free bike status
  * @param rawResponse - Should return all response or just data
  * @returns Defined free bike status
  */
-const getFreeBikeStatus = async (rawResponse: boolean = false) => {
-  return makeRequest({
-    apiUrl: 'https://data-sharing.tier-services.io/tier_paris/gbfs/2.2/free-bike-status',
+const getFreeBikeStatus = async (rawResponse: boolean = false) =>
+  makeRequest({
+    apiUrl: `${API_BASE_URL}/free-bike-status`,
     cacheKey: 'get-free-bike-status',
     rawResponse,
   });
-};
 
 export { getServices, getPricingPlans, getFreeBikeStatus };
